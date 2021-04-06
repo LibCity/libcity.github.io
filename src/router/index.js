37582f6d081b51bf6969eea37aedb145b2bb3273
NickHan <chengkaihan1019@outlook.com>
2021-04-06 14:31:13 +0800

    no history mode

--- src/router/index.js
+++ src/router/index.js
@@ -135,7 +135,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   routes,
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
