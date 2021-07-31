dd9af31ac380d64c8aa9fd2238d6ec64fd38f54f
NickHan-cs <1152090685@qq.com>
2021-07-31 20:35:04 +0800

    fix platform Demo link

--- src/views/home/platformCard.vue
+++ src/views/home/platformCard.vue
@@ -46,7 +46,7 @@
         <div class="pcard">
             <a-card title="Demo" :bordered="true" style="border-radius: 5px">
                 <p style="margin-bottom: 0px; height: 120px">This website provides a demonstration video for researchers to know the basic usage of LibTraffic quickly.</p>
-                <a-button style="padding-left: 180px; margin-top: 5px; font-size: 15px;" type="link" @click="toVideo">See More >></a-button>
+                <a-button style="padding-left: 180px; margin-top: 5px; font-size: 15px;" type="link" @click="toDemo">See More >></a-button>
             </a-card>
         </div>
       </a-col>
@@ -81,9 +81,9 @@ export default {
             name: "Chart",
         });
     },
-    toVideo() {
+    toDemo() {
         this.$router.push({
-            name: "Video",
+            name: "Demo",
         });
     }
   }
